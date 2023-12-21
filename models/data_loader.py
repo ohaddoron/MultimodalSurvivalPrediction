5bf8dc57a28e8f32bc10cb4c11c954789f881ef6
Ohad Doron <ogdoron@gmail.com>
2023-12-21 11:03:09 +0200

    code running e2e with model saving

--- models/data_loader.py
+++ models/data_loader.py
@@ -1,4 +1,6 @@
 """MyDataset"""
+import json
+from pathlib import Path
 
 import pandas as pd
 import numpy as np
@@ -7,150 +9,132 @@ from torch.utils.data.sampler import SubsetRandomSampler
 
 
 def FindOutIndex(data):
-	out_idx = []
-	target = data.values.tolist()
-	for i in range(data.shape[0]):
-		try:
-			target_ex = np.array(target[i], dtype='int')
-		except:
-			out_idx.append(i)
-	return out_idx
+    out_idx = []
+    target = data.values.tolist()
+    for i in range(data.shape[0]):
+        try:
+            target_ex = np.array(target[i], dtype='int')
+        except:
+            out_idx.append(i)
+    return out_idx
+
+
+def get_test_indices_based_on_test_patients_file_and_clinical_data(clinical_data):
+    test_patients = set(json.loads(Path(__file__).parent.parent.joinpath('test_patients.json').read_text()))
+
+    test_indices = []
+
+    for index, row in clinical_data.iterrows():
+        if row[0] in test_patients:
+            test_indices.append(index)
+    return test_indices
 
 
 def preprocess_clinical_data(clinical_path):
-	data_clinical = pd.read_csv(clinical_path, header=None)
-	target_data = data_clinical[[6, 7]]
-	out_idx = FindOutIndex(target_data)
+    data_clinical = pd.read_csv(clinical_path, header=None)
+    test_indices = get_test_indices_based_on_test_patients_file_and_clinical_data(clinical_data=data_clinical)
 
-	clin_variables = data_clinical[[1, 2, 3, 4, 5]]
-	idx = clin_variables[clin_variables[5].isnull()].index
-	g = [i for i in idx]
+    target_data = data_clinical[[6, 7]]
+    out_idx = FindOutIndex(target_data)
 
-	data_clinical.drop(index=out_idx+g, inplace=True)
+    clin_variables = data_clinical[[1, 2, 3, 4, 5]]
+    idx = clin_variables[clin_variables[5].isnull()].index
+    g = [i for i in idx]
 
-	target_data = data_clinical[[6, 7]]
-	clin_data_categorical = data_clinical[[1, 2, 3, 4]]
-	clin_data_continuous = data_clinical[[5]]
+    data_clinical.drop(index=out_idx + g + test_indices, inplace=True)
 
-	return clin_data_categorical, clin_data_continuous, target_data, out_idx+g
+    target_data = data_clinical[[6, 7]]
+    clin_data_categorical = data_clinical[[1, 2, 3, 4]]
+    clin_data_continuous = data_clinical[[5]]
+
+    return clin_data_categorical, clin_data_continuous, target_data, out_idx + g + test_indices
 
 
 class MyDataset(torch.utils.data.Dataset):
-	def __init__(self, modalities, data_path):
-		"""
-		Parameters
-		----------
-		modalities: list
-			Used modalities
+    def __init__(self, modalities, data_path):
+        """
+        Parameters
+        ----------
+        modalities: list
+            Used modalities
 
-		data_path: dict
-			The path of used data.
+        data_path: dict
+            The path of used data.
 
-		Returns
-		-------
-		data: dictionary
-			{'clin_data_categorical': ,..,'mRNA': ...}
+        Returns
+        -------
+        data: dictionary
+            {'clin_data_categorical': ,..,'mRNA': ...}
 
-		data_label: dictionary
-			{'label':[[event, time]]}
-		"""
-		super(MyDataset, self).__init__()
-		self.data_modalities = modalities
-		# label
-		clin_data_categorical, clin_data_continuous, target_data, remove_idx = preprocess_clinical_data(data_path['clinical'])
-		self.target = target_data.values.tolist()
+        data_label: dictionary
+            {'label':[[event, time]]}
+        """
+        super(MyDataset, self).__init__()
+        self.data_modalities = modalities
+        # label
+        clin_data_categorical, clin_data_continuous, target_data, remove_idx = preprocess_clinical_data(
+            data_path['clinical'])
+        self.target = target_data.values.tolist()
 
-		# clinical
-		if 'clinical' in self.data_modalities:
-			self.clin_cat = clin_data_categorical.values.tolist()
-			self.clin_cont = clin_data_continuous.values.tolist()
+        # clinical
+        if 'clinical' in self.data_modalities:
+            self.clin_cat = clin_data_categorical.values.tolist()
+            self.clin_cont = clin_data_continuous.values.tolist()
 
-		# mRNA
-		if 'mRNA' in self.data_modalities:
-			data_mrna = pd.read_csv(data_path['mRNA'], header=None)
-			data_mrna.drop(index=remove_idx, inplace=True)
-			self.data_mrna = data_mrna.values.tolist()
+        # mRNA
+        if 'mRNA' in self.data_modalities:
+            data_mrna = pd.read_csv(data_path['mRNA'], header=None)
+            data_mrna.drop(index=remove_idx, inplace=True)
+            self.data_mrna = data_mrna.values.tolist()
 
-		# miRNA
-		if 'miRNA' in self.data_modalities:
-			data_mirna = pd.read_csv(data_path['miRNA'], header=None)
-			data_mirna.drop(index=remove_idx, inplace=True)
-			self.data_mirna = data_mirna.values.tolist()
+        # miRNA
+        if 'miRNA' in self.data_modalities:
+            data_mirna = pd.read_csv(data_path['miRNA'], header=None)
+            data_mirna.drop(index=remove_idx, inplace=True)
+            self.data_mirna = data_mirna.values.tolist()
 
-		# CNV
-		if 'CNV' in self.data_modalities:
-			data_cnv = pd.read_csv(data_path['CNV'], header=None)
-			data_cnv.drop(index=remove_idx, inplace=True)
-			self.data_cnv = data_cnv.values.tolist()
+        # CNV
+        if 'CNV' in self.data_modalities:
+            data_cnv = pd.read_csv(data_path['CNV'], header=None)
+            data_cnv.drop(index=remove_idx, inplace=True)
+            self.data_cnv = data_cnv.values.tolist()
 
-	def __len__(self):
-		return len(self.clin_cat)
+    def __len__(self):
+        return len(self.clin_cat)
 
-	def __getitem__(self, index):
-		data = {}
-		data_label = {}
-		target_y = np.array(self.target[index], dtype='int')
-		target_y = torch.from_numpy(target_y)
-		data_label['label'] = target_y.type(torch.LongTensor)
+    def __getitem__(self, index):
+        data = {}
+        data_label = {}
+        try:
+            target_y = np.array(self.target[index], dtype='int')
+        except IndexError as ie:
+            raise ie
 
-		
-		if 'clinical' in self.data_modalities:
-			clin_cate = np.array(self.clin_cat[index]).astype(np.int64)
-			clin_cate = torch.from_numpy(clin_cate)
-			data['clinical_categorical'] = clin_cate
+        target_y = torch.from_numpy(target_y)
+        data_label['label'] = target_y.type(torch.LongTensor)
 
-			clin_conti = np.array(self.clin_cont[index]).astype(np.float32)
-			clin_conti = torch.from_numpy(clin_conti)
-			data['clinical_continuous'] = clin_conti
+        if 'clinical' in self.data_modalities:
+            clin_cate = np.array(self.clin_cat[index]).astype(np.int64)
+            clin_cate = torch.from_numpy(clin_cate)
+            data['clinical_categorical'] = clin_cate
 
+            clin_conti = np.array(self.clin_cont[index]).astype(np.float32)
+            clin_conti = torch.from_numpy(clin_conti)
+            data['clinical_continuous'] = clin_conti
 
-		if 'mRNA' in self.data_modalities:
-			mrna = np.array(self.data_mrna[index])
-			mrna = torch.from_numpy(mrna)
-			data['mRNA'] = mrna.type(torch.FloatTensor)
+        if 'mRNA' in self.data_modalities:
+            mrna = np.array(self.data_mrna[index])
+            mrna = torch.from_numpy(mrna)
+            data['mRNA'] = mrna.type(torch.FloatTensor)
 
+        if 'miRNA' in self.data_modalities:
+            mirna = np.array(self.data_mirna[index])
+            mirna = torch.from_numpy(mirna)
+            data['miRNA'] = mirna.type(torch.FloatTensor)
 
-		if 'miRNA' in self.data_modalities:
-			mirna = np.array(self.data_mirna[index])
-			mirna = torch.from_numpy(mirna)
-			data['miRNA'] = mirna.type(torch.FloatTensor)
+        if 'CNV' in self.data_modalities:
+            cnv = np.array(self.data_cnv[index])
+            cnv = torch.from_numpy(cnv)
+            data['CNV'] = cnv.type(torch.FloatTensor)
 
-		if 'CNV' in self.data_modalities:
-			cnv = np.array(self.data_cnv[index])
-			cnv = torch.from_numpy(cnv)
-			data['CNV'] = cnv.type(torch.FloatTensor)
-
-		return data, data_label
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        return data, data_label
--- models/utils.py
+++ models/utils.py
@@ -8,137 +8,115 @@ import torch
 from torch.utils.data.sampler import SubsetRandomSampler
 from torch.utils.data import DataLoader
 
-
 # The dictionary of the input data path
 DATA_PATH = {'clinical': '/opt/MultimodalSurvivalPrediction/preprocess/data/Pc_clinical_emb.csv',
-			 'mRNA': '/opt/MultimodalSurvivalPrediction/preprocess/data/PC_mRNA_threshold_7.csv',
-			 'miRNA': '/opt/MultimodalSurvivalPrediction/preprocess/data/PC_miRNA.csv',
-			 'CNV': '/opt/MultimodalSurvivalPrediction/preprocess/data/PC_CNV_threshold_20.csv'}
+             'mRNA': '/opt/MultimodalSurvivalPrediction/preprocess/data/PC_mRNA_threshold_7.csv',
+             'miRNA': '/opt/MultimodalSurvivalPrediction/preprocess/data/PC_miRNA.csv',
+             'CNV': '/opt/MultimodalSurvivalPrediction/preprocess/data/PC_CNV_threshold_20.csv'}
 
 
 def setup_seed(seed):
-	"""
-	Set random seed for torch.
-	"""
-	torch.manual_seed(seed)
-	torch.cuda.manual_seed(seed)
-	np.random.seed(seed)
-	torch.backends.cudnn.deterministic = True
+    """
+    Set random seed for torch.
+    """
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    np.random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+
 
 def test_gpu():
-	"""
-	Detect the hardware: GPU or CPU?
-	"""
-	print('GPU？', torch.cuda.is_available())
-	device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-	print('The device is ', device)
-	return device
+    """
+    Detect the hardware: GPU or CPU?
+    """
+    print('GPU？', torch.cuda.is_available())
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print('The device is ', device)
+    return device
+
 
 def evaluate_model(c_index_arr):
-	"""
-	Calculate the Mean and the Standard Deviation about c-index array.
-	"""
-	m = np.sum(c_index_arr, axis=0) / len(c_index_arr)
-	s = np.std(c_index_arr)
-	return m, s
-
+    """
+    Calculate the Mean and the Standard Deviation about c-index array.
+    """
+    m = np.sum(c_index_arr, axis=0) / len(c_index_arr)
+    s = np.std(c_index_arr)
+    return m, s
 
 
 def get_dataloaders(mydataset, train_sampler, val_sampler, test_sampler, batch_size):
-	"""
-	Parameters
-	----------
-	mydataset: Dataset
+    """
+    Parameters
+    ----------
+    mydataset: Dataset
 
-	train_sampler: array
-		Patient indexs in train set.
+    train_sampler: array
+        Patient indexs in train set.
 
-	val_sampler: array
-		Patient indexs in validation set.
+    val_sampler: array
+        Patient indexs in validation set.
 
-	test_sampler: array
-		Patient indexs in test set.
+    test_sampler: array
+        Patient indexs in test set.
 
-	batch_size: int
-		Number of patients in each batch.
+    batch_size: int
+        Number of patients in each batch.
 
-	Return
-	------
-	A dictionary of train/validation/test set, like
-		{'train': train_loader, 'val': val_loader, 'test': test_loader}
-	"""
-	
-	dataloaders = {}
-	dataloaders['train'] = DataLoader(mydataset, batch_size=batch_size, sampler=SubsetRandomSampler(train_sampler))
-	dataloaders['val'] = DataLoader(mydataset, batch_size=len(val_sampler), sampler=SubsetRandomSampler(val_sampler))
-	dataloaders['test'] = DataLoader(mydataset, batch_size=len(test_sampler), sampler=SubsetRandomSampler(test_sampler))
+    Return
+    ------
+    A dictionary of train/validation/test set, like
+        {'train': train_loader, 'val': val_loader, 'test': test_loader}
+    """
 
-	print('Dataset sizes (# patients):')
-	print('train: ', len(train_sampler))
-	print('  val: ', len(val_sampler))
-	print(' test: ', len(test_sampler))
-	print()
-	print('Batch size: ', batch_size)
+    dataloaders = {}
+    dataloaders['train'] = DataLoader(mydataset, batch_size=batch_size, sampler=SubsetRandomSampler(train_sampler),
+                                      num_workers=os.cpu_count())
+    dataloaders['val'] = DataLoader(mydataset, batch_size=len(val_sampler), sampler=SubsetRandomSampler(val_sampler))
+    dataloaders['test'] = DataLoader(mydataset, batch_size=len(test_sampler), sampler=SubsetRandomSampler(test_sampler))
 
-	return dataloaders
+    print('Dataset sizes (# patients):')
+    print('train: ', len(train_sampler))
+    print('  val: ', len(val_sampler))
+    print(' test: ', len(test_sampler))
+    print()
+    print('Batch size: ', batch_size)
+
+    return dataloaders
 
 
 def compose_run_tag(model, lr, dataloaders, log_dir, suffix=''):
-	"""
-	Make the tag about modality and learning rate.
-	"""
-	def add_string(string, addition, sep='_'):
-		if not string:
-			return addition
-		else: return string + sep + addition
+    """
+    Make the tag about modality and learning rate.
+    """
 
-	data = None
-	for modality in model.data_modalities:
-		data = add_string(data, modality)
+    def add_string(string, addition, sep='_'):
+        if not string:
+            return addition
+        else:
+            return string + sep + addition
 
-	run_tag = f'{data}_lr{lr}'
+    data = None
+    for modality in model.data_modalities:
+        data = add_string(data, modality)
 
-	run_tag += suffix
+    run_tag = f'{data}_lr{lr}'
 
-	print(f'Run tag: "{run_tag}"')
+    run_tag += suffix
 
-	tb_log_dir = os.path.join(log_dir, run_tag)
+    print(f'Run tag: "{run_tag}"')
 
-	return run_tag
+    tb_log_dir = os.path.join(log_dir, run_tag)
+
+    return run_tag
 
 
 def save_5fold_results(c_index_arr, run_tag):
-	"""
-	Save the results after 5 fold cross validation.
-	"""
-	m, s = evaluate_model(c_index_arr)
-	with open(f'proposed_{run_tag}.txt', 'w') as file:
-		file.write(str(c_index_arr))
-		file.write(f"\n Mean: {m}")
-		file.write(f"\n Std: {s}")
-	file.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    """
+    Save the results after 5 fold cross validation.
+    """
+    m, s = evaluate_model(c_index_arr)
+    with open(f'proposed_{run_tag}.txt', 'w') as file:
+        file.write(str(c_index_arr))
+        file.write(f"\n Mean: {m}")
+        file.write(f"\n Std: {s}")
+    file.close()
